264b8bd001302b2da40fd831bec90cfa559e5541
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-02-01 16:27:33 -0500

    commit 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+//variables para grupo
 let minGrupo = 0;
 let maxGrupo = 3;
 let grupo = [];
 let contGrupo = 1;
-const jsonGrupos = ["BEBES","NIÑOS","ADULTOS",];
+const jsonGrupos = ["BEBES","NIÑOS","ADULTOS"];
 
+//variables para largo pierna
 var largoPierna = [];
 let contPierna = 1;
 const jsonVarsLargoPierna = {
@@ -12,6 +14,8 @@ const jsonVarsLargoPierna = {
   "3": { "largoPiernaMin": 9, "largoPiernaMax": 30 },
 };
 
+
+//variables para largo puño
 const largoPuno = [];
 let contPuno = 1;
 const jsonVarsLargoPuno = {
@@ -21,17 +25,15 @@ const jsonVarsLargoPuno = {
 };
 
 
-// Grupo
-
+// For para Grupo
 for (let y = minGrupo ; y <= maxGrupo ; y++) {
     tJsonGrupo = {id:contGrupo ++, name: jsonGrupos[y]};
     grupo.push(tJsonGrupo);
     delete(grupo[3]);
   }
   
-  console.log(grupo);
 
-// Para largo pierna
+// For para largo pierna
 for (let keyPierna in jsonVarsLargoPierna) {
   var tLargoPiernaMin = jsonVarsLargoPierna[keyPierna]["largoPiernaMin"];
   var tLargoPiernaMax = jsonVarsLargoPierna[keyPierna]["largoPiernaMax"];
@@ -42,7 +44,7 @@ for (let keyPierna in jsonVarsLargoPierna) {
   }
 }
 
-//  Para largo puño
+//  For para largo puño
 for (let keyPuno in jsonVarsLargoPuno) {
   var tLargoPunoMin = jsonVarsLargoPuno[keyPuno]["largoPunoMin"];
   var tLargoPunoMax = jsonVarsLargoPuno[keyPuno]["largoPunoMax"];
@@ -54,28 +56,8 @@ for (let keyPuno in jsonVarsLargoPuno) {
 }
 
 
-// function myOnLoad() {
-//     cargar();
-//    }
-// // funcion para Cargar  campo <select>
-// function cargar() {
-//   jsonGrupos.sort();
-
-//   addOptions("grupo", grupo[1]['name']);
-// }
-
-// //  agregar opciones a un <select>
-// function addOptions(domElement, array) {
-//   var select = document.getElementsByName(domElement)[0];
-
-//   for (value in array) {
-//     var option = document.createElement("option");
-//     option.text = array[value];
-//     select.add(option);
-//   }
-// }
-
-
+console.log("------------------------ grupo ----------------------");
+console.log(grupo);
 
 console.log("------------------------ Largo pierna ----------------------");
 console.log(resultadoPierna);
@@ -83,3 +65,32 @@ console.log(resultadoPierna);
 console.log("------------------------ Largo puño ----------------------");
 console.log(resultadoPuno);
 
+// Mapeamos los jsons para traer los ids
+grupoMap = grupo.filter((x) => x !== null).map(x => x.id );
+piernaMap = resultadoPierna.filter((x) => x !== null).map(x => x.groupId );
+punoMap = resultadoPuno.filter((x) => x !== null).map(x => x.groupId );
+
+console.log(grupoMap[0]);
+
+
+//mostrar las variables de acuerdo a ese id
+if(grupoMap[0] == 1){
+     resultado = 'Grupo: '+ grupoMap[0] + ', Pierna: '+ piernaMap.slice(0,10);
+     console.log(resultado);
+}else{
+    console.log('error');
+}
+
+if(grupoMap[1] == 2){
+    resultado = 'Grupo: '+ grupoMap[1] + ', Pierna: '+ piernaMap.slice(10,33);
+    console.log(resultado);
+}else{
+    console.log('error');
+}
+
+if(grupoMap[2] == 3){
+    resultado = 'Grupo: '+ grupoMap[2] + ', Pierna: '+ piernaMap.slice(33,55);
+    console.log(resultado);
+}else{
+    console.log('error');
+}
